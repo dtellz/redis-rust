d201b36fb098ab1da8af26efad4a84914e7826ff
Diego <dtellez95@gmail.com>
2022-10-12 22:50:46 +0100

    refactor server on set up

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,8 @@ use std::net::TcpListener;
 use std::io::prelude::*;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    println!("[+] Redis server listening on port 6379...");
     loop {
         match listener.accept() {
             Ok((_socket, addr)) => {
